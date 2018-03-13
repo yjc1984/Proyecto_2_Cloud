0672401031afb368d229c40675e975a44534fa1b
YEISON JAVIER <yjc1984@gmail.com>
2018-03-12 20:48:48 -0500

    Ajuste variables entorno

--- concursos/settings.py
+++ concursos/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vxceyeh+z9btl#&xdwnv)7ll*%=l1$l)v69^n&ko!6&e-$!5bj'
+SECRET_KEY = os.environ['VAR_SEC_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.153.157.165','127.0.0.1','localhost']
+ALLOWED_HOSTS = os.environ['VAR_HOST_AWS']
 
 
 # Application definition
