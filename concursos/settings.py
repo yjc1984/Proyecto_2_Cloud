855cf02349f0679fc34bc35bdbbbce48bc73b6c9
YEISON JAVIER <yjc1984@gmail.com>
2018-03-12 21:07:06 -0500

    Ajuste variables entorno

--- concursos/settings.py
+++ concursos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['VAR_SEC_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ['VAR_HOST_AWS']
+ALLOWED_HOSTS = [os.environ['VAR_HOST_AWS']]
 
 
 # Application definition
