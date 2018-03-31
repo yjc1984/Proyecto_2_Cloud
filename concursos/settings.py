37751c0b03d6470d1fee76b7b2e4be1f3a80a05f
YEISON JAVIER <yjc1984@gmail.com>
2018-03-31 16:48:04 -0500

    Adicion almacenamiento s3

--- concursos/settings.py
+++ concursos/settings.py
@@ -171,4 +171,4 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'WebConcursos.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
